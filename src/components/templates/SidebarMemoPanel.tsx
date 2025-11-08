4f6175c1c208eeb0211f410acf62224a7f85c664
tamai-hideyuki <hide27yuki@gmail.com>
2025-11-09 07:47:21 +0900

    ✨ templates/SidebarMemoPanel.tsx を作成

---
+++ src/components/templates/SidebarMemoPanel.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import Heading from '../atoms/Heading';
+import Button from '../atoms/Button';
+import MemoList from '../organisms/MemoList';
+import type { Memo } from '../../types';
+
+type SidebarMemoPanelProps = {
+  memos: Memo[];
+  selectedId: string | null;
+  onSelect: (id: string) => void;
+  onAdd: () => void;
+  onReorder: (from: number, to: number) => void;
+};
+
+const SidebarMemoPanel: React.FC<SidebarMemoPanelProps> = ({
+  memos,
+  selectedId,
+  onSelect,
+  onAdd,
+  onReorder,
+}) => {
+  return (
+    <aside className="flex flex-col justify-between w-60 border-r">
+      <div className="p-3">
+        <Heading level={2} className="text-lg mb-2">
+          メモ一覧
+        </Heading>
+        <MemoList memos={memos} selectedId={selectedId} onSelect={onSelect} onReorder={onReorder} />
+      </div>
+      <div className="p-3">
+        <Button type="button" onClick={onAdd} aria-label="メモを追加">
+          追加
+        </Button>
+      </div>
+    </aside>
+  );
+};
+
+export default SidebarMemoPanel;
